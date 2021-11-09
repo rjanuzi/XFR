cf970615c23e7598a4254fc7c7856b69c22b8ee0
Rafael B. Januzi <rjanuzi@gmail.com>
2021-11-08 23:45:34 -0300

    Removing old drive URLs

--- dataset/download.py
+++ dataset/download.py
@@ -3,15 +3,9 @@ import gdown
 from raw_ds import RAW_DATASET_FOLDER
 from processed_ds import PROCESSED_DATASET_FOLDER
 
-DATASET_RAW_DRIVE_SHARED_FOLDER = (
-    # "https://drive.google.com/drive/folders/1C5XZIPeARoVjotVdYCNRVqolIFOGV6BU"
-    "https://drive.google.com/drive/folders/1kXjCPA-WGGhP33WKBa-dmSEdtCwkPLXD?usp=sharing"
-)
+DATASET_RAW_DRIVE_SHARED_FOLDER = "https://drive.google.com/drive/folders/1kXjCPA-WGGhP33WKBa-dmSEdtCwkPLXD?usp=sharing"
 
-DATASET_PROCESSED_DRIVE_SHARED_FOLDER = (
-    # "https://drive.google.com/drive/folders/1C5XZIPeARoVjotVdYCNRVqolIFOGV6BU"
-    "https://drive.google.com/drive/folders/1RDBurXFlOeSri6fONd3V3Q_PqGkkjNve?usp=sharing"
-)
+DATASET_PROCESSED_DRIVE_SHARED_FOLDER = "https://drive.google.com/drive/folders/1RDBurXFlOeSri6fONd3V3Q_PqGkkjNve?usp=sharing"
 
 
 def download_raw_folder():
