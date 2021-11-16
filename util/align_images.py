061e47f65de5b310b6afb2d511c299886b5c719d
Rafael B. Januzi <rjanuzi@gmail.com>
2021-11-16 00:24:29 -0300

    Adding some status messages

--- util/align_images.py
+++ util/align_images.py
@@ -11,22 +11,32 @@ LANDMARKS_MODEL_FNAME = "shape_predictor_68_face_landmarks.dat.bz2"
 
 
 def unpack_bz2(src_path):
+    print("Unpacking landmarks file...")
+
     dst_path = Path(src_path.parent, src_path.stem)
     if not dst_path.exists():
         data = bz2.BZ2File(src_path).read()
         with open(dst_path, "wb") as fp:
             fp.write(data)
 
+    print("done.")
+
     return dst_path
 
 
 def get_landmark_model(fname=LANDMARKS_MODEL_FNAME, url=LANDMARKS_MODEL_URL):
+    print(
+        "Downloading landmark model from http://dlib.net/files/shape_predictor_68_face_landmarks.dat.bz2.."
+    )
+
     file = Path(LANDMARKS_TEMP_FOLDER, fname)
     if not file.exists():
         LANDMARKS_TEMP_FOLDER.mkdir(parents=True, exist_ok=True)
         r = requests.get(url)
         with open(file, "wb") as fp:
             fp.write(r.content)
+
+    print("done.")
 
     return unpack_bz2(file)
 
@@ -38,9 +48,11 @@ def align_images(imgs_path_lst, output_folder_path_lst):
     :param output_path_lst: list of paths to output images
     """
     landmarks_detector = LandmarksDetector(get_landmark_model())
+    print("Aligning images...")
     for img_path, output_folder in zip(imgs_path_lst, output_folder_path_lst):
         for i, face_landmarks in enumerate(
             landmarks_detector.get_landmarks(img_path), start=1
         ):
             output_img_path = output_folder.joinpath(f"{img_path.stem}_{i}.png")
             image_align(img_path, output_img_path, face_landmarks)
+    print("done.")
