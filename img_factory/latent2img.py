650a9fca91afec6af6f962fbc19c5bc7c0007f2c
Rafael B. Januzi <rjanuzi@gmail.com>
2021-11-28 22:04:59 -0300

    Small fix in latent2img

--- img_factory/latent2img.py
+++ img_factory/latent2img.py
@@ -25,16 +25,17 @@ def generate(person_name: str) -> Image:
 
 
 def add_original_background(person_name: str, generated_img: Image) -> Image:
-    imask = read_mask(person_name=PERSON)
-    aligned = read_aligned(person_name=PERSON)
+    imask = read_mask(person_name=person_name)
+    aligned = read_aligned(person_name=person_name)
 
+    # Process the mask image
     width, height = aligned.size
     imask = imask.resize((width, height))
     imask = imask.filter(ImageFilter.GaussianBlur(radius=8))
     mask = np.array(imask) / 255
     mask = np.expand_dims(mask, axis=-1)
 
-    # Composite the background from original with the generated image
+    # Composite the background (with hair) from original with the generated image
     background = (1.0 - mask) * np.array(aligned)
     generated_face = mask * np.array(generated_img)
     img_array = generated_face + background
