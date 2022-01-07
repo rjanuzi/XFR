70d7c1b0d2dc25ff2eb74ab77971f983a3a51451
Rafael B. Januzi <rjanuzi@gmail.com>
2022-01-07 19:21:09 -0300

    New sample

--- imgs_morph_sample.py
+++ imgs_morph_sample.py
@@ -8,9 +8,10 @@ OUTPUT_FOLDER = Path(".morph_samples", f"{datetime.now().strftime('%Y%m%d_%H%M%S
 OUTPUT_FOLDER.mkdir(parents=True, exist_ok=True)
 
 
-NAME_1 = "25169"
-NAME_2 = "25711"
+NAME_1 = "25001"
+NAME_2 = "25007"
 MASK_TO_APPLY = NAME_1
+MASK_TO_APPLY = None
 
 generated_imgs = morph(name_1=NAME_1, name_2=NAME_2, mask_to_apply=MASK_TO_APPLY)
 
