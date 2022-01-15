6bae3f357a1b9cd2fd5768e0b2760c2d450fb3bd
Rafael B. Januzi <rjanuzi@gmail.com>
2022-01-15 19:25:22 -0300

    Adding some prints to morph

--- run_morph.py
+++ run_morph.py
@@ -2,6 +2,7 @@ import traceback
 from time import time
 
 from dataset import (
+    DATASET_KIND_ALIGNED,
     DATASET_KIND_LATENTS,
     DATASET_KIND_MORPH,
     DATASET_KIND_STR,
@@ -12,6 +13,8 @@ from img_factory.imgs_morph import morph
 from util._telegram import send_gif, send_simple_message
 from util.gif import imgs_to_gif
 
+__MAX_MORPHS = 200
+
 # Get the list of images to align, ignoring the already aligned ones
 dataset_idx = gen_dataset_index()
 latents_dataset = dataset_idx.loc[
@@ -20,6 +23,7 @@ latents_dataset = dataset_idx.loc[
 
 # Split the dataset at mid point (we want to morph the imgs at index n with n+mid)
 latents_dataset = latents_dataset.sort_values(by=["name"]).reset_index(drop=True)
+latents_dataset = latents_dataset.iloc[:__MAX_MORPHS]
 mid = int(len(latents_dataset) / 2)
 latents_dataset_1 = latents_dataset.iloc[:mid].reset_index(drop=True)
 latents_dataset_2 = latents_dataset.iloc[mid:].reset_index(drop=True)
@@ -30,6 +34,7 @@ latents_to_morph = latents_dataset_1.join(
 
 done_count = 0
 start_time = time()
+print(f"Starting morphs (total: {len(latents_to_morph)})")
 for _, row in latents_to_morph.iterrows():
     step_start_time = time()
     source_a_name = row.name_1
@@ -71,7 +76,7 @@ for _, row in latents_to_morph.iterrows():
             print(f"Failed to morph {source_a_name} with {source_b_name}")
 
         done_count += 1
-        if done_count % 20 == 0:
+        if done_count % 10 == 0:
             print(
                 f"Morphing... {done_count}/{len(latents_to_morph)} -- ({round((done_count/len(latents_to_morph))*100.0,2)}%) -- {int((time()-start_time)/60)} min -- {int(time()-step_start_time)} sec / pair"
             )
