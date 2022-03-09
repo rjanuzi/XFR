b4698e6c5708482e57ff3f72748b65b65ef48215
Rafael B. Januzi <rjanuzi@gmail.com>
2022-03-09 00:46:57 -0300

    Adding dlib face recognition example

--- run_dlib_fr_sample.py
+++ run_dlib_fr_sample.py
@@ -10,8 +10,8 @@ dlib_fr = DlibFr()
 features_1 = dlib_fr.gen_features(person_1_path)
 features_2 = dlib_fr.gen_features(person_2_path)
 
-distancy = dlib_fr.calc_distance(person_1_path, person_2_path)
+distance = dlib_fr.calc_distance(person_1_path, person_2_path)
 
 is_same = dlib_fr.check(person_1_path, person_2_path, TOLERANCE)
 
-print(f"distancy: {round(distancy,2)}. Is the same person: {is_same}.")
+print(f"distance: {round(distance,2)}. Is the same person: {is_same}.")
